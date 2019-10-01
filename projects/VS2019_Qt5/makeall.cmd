5c583ae753641039f6bb4cb33314a3517566deb2
sv <svpro@outlook.com>
2019-10-01 04:00:49 +0300

    Removed build step for the spider_task_creator from the build script for VS 2019

--- projects/VS2019_Qt5/makeall.cmd
+++ projects/VS2019_Qt5/makeall.cmd
@@ -10,4 +10,3 @@ call %MAKER% qnetmap.sln qnetmap_universaltile_map %1
 
 call %MAKER% qnetmap.sln mapsviewer %1
 call %MAKER% qnetmap.sln mapsspider %1
-call %MAKER% qnetmap.sln spider_task_creator %1
